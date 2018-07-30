b029fda9d5a9783a084e056482c1f900a9369a20
robcmills <rob@buildingconnected.com>
2018-07-29 17:30:55 -0700

    Clean up app container

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Navigation from '../components/navigation';
 import Message from '../containers/Message';
-import styles from '../css/main';
+import styles from '../css/main.css';
 
 const cx = classNames.bind(styles);
 
-
-/*
- * React-router's <Router> component renders <Route>'s
- * and replaces `this.props.children` with the proper React Component.
- *
- * Please refer to `routes.jsx` for the route config.
- *
- * A better explanation of react-router is available here:
- * https://github.com/rackt/react-router/blob/latest/docs/Introduction.md
- */
 const App = ({ children }) => {
   return (
     <div className={cx('app')}>
@@ -25,10 +14,6 @@ const App = ({ children }) => {
       {children}
     </div>
   );
-};
-
-App.propTypes = {
-  children: PropTypes.object
 };
 
 export default App;
